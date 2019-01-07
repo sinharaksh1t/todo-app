b35c6d7f9ae600eb87d13f4dd0724344d3ae5be7
Rakshit Sinha <rakshit.sinha@oracle.com>
2019-01-07 01:28:35 -0800

    Created app component

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Items from "./components/Items/Items";
+
+const App = () => {
+  return (
+    <Items />
+  );
+}
+
+export default App;
---
+++ src/components/Items/Items.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const items = () => {
+  return (
+    <div>
+        <input type="checkbox" /> I have a bike <br></br>
+        <input type="checkbox" /> I have a car <br></br>
+        <input type="checkbox" /> I have a boat <br></br>
+    </div>
+  );
+}
+
+export default items;
