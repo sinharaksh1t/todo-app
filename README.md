01456b4dd2048504059245f3d64ae8b9239adb90
Rakshit Sinha <rakshit.sinha@oracle.com>
2019-01-08 01:12:11 -0800

    Created a basic directory structure and added inline styling to starter components

---
+++ README.md
@@ -0,0 +1,2 @@
+# Todo-List App (React.js)
+A Todo-list app made with React.
---
+++ src/components/Header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const header = () => {
+    return (
+        <header className = "header">
+            This is a header.
+        </header>
+    );
+};
+
+export default header;
